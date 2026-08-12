bf821d9c1b08b8d6f9a42954b0f883ad7a8684c4
KSamtoclef <samtoclef@gmail.com>
2026-08-12 12:37:27 +0100

    Align professional release cache version

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -1,6 +1,6 @@
 import{SUPABASE_URL,SUPABASE_ANON_KEY}from'./app-config.js';
 
-export const RELEASE_VERSION='20260809-visits-only-r1';
+export const RELEASE_VERSION='20260812-professional-r3';
 const stylePromises=new Map(),modulePromises=new Map();
 
 function loadStyle(href,key){
